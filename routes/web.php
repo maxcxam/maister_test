b98cc27f57af2ddae98ed2375cfbd3f35346fd07
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-08 20:56:58 +0300

    Add invoice functionality and UI improvements
    Introduced 'Status' enum and relationships for invoices in models. Updated user interface for better user accessibility and added invoice display in user view. Upgraded Laravel framework and Tailwind configuration for improved performance.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('users.index');
 });
 
 Route::get('/refresh', function () {
