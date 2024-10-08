30a817b8ed7527712748d58ce1c42416eaad8c71
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-08 13:50:04 +0300

    First commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,5 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         Order::factory(100)->create();
-
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/refresh', function () {
+    Artisan::call('migrate:fresh --seed');
+    return "Database has been refreshed";
+});
+
