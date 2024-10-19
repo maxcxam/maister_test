478322f41a8bed518b6b5d3af870c737757c0a33
roman <romandzindzyura@gmail.com>
2024-10-19 17:08:36 +0300

    Add dropdown menu for selecting reasons
    Replaced placeholder text with a select dropdown to allow users to choose a specific reason. This menu includes options for reassignment, upsell, second service, and second contact reasons.

--- resources/views/bitrix/apps/handler.blade.php
+++ resources/views/bitrix/apps/handler.blade.php
@@ -3,5 +3,11 @@
 </head>
 <body>
 @dump($result)
-ololo
+<select name="reason" id="reason" class="form-select">
+    <option value="" disabled selected>Оберіть причину</option>
+    <option value="master_reassigned">Перекріпити майстра (чи закріпити нового майстра)</option>
+    <option value="upsell">Доппродана угода (менеджер ще допродав якусь послугу)</option>
+    <option value="second_service">Друга послуга (під час першого звернення клієнту потрібен ремонт не однієї одиниці техніки)</option>
+    <option value="second_contact">Друге звернення клієнта (клієнт звертався раніше, і зараз звернувся повторно за новою послугою)</option>
+</select>
 </body>
