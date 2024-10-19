8d8165a80557f59a2b5e8e1d88939eb2cab0b665
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 12:01:10 +0300

    Update Request import in MainAppController
    Replaced 'Illuminate\Support\Facades\Request' with 'Illuminate\Http\Request' to ensure correct request handling and avoid potential namespace conflicts. This change will improve the code's readability and maintainability.

--- app/Http/Controllers/Bitrix/Apps/MainAppController.php
+++ app/Http/Controllers/Bitrix/Apps/MainAppController.php
@@ -12,13 +12,13 @@ class MainAppController extends Controller
     public function install(Request $request)
     {
         $result = CRest::installApp();
-        Log::info('Request was handled', ['request' => $request->all(), 'result'=>'ok']);
+        Log::info('install: Request was handled', ['request' => $request->all(), 'result'=>'ok']);
         return view('bitrix.apps.index', compact('result'));
     }
 
     public function handler(Request $request)
     {
-        Log::info('Request was handled', ['request' => $request->all(), 'result'=>'ok']);
+        Log::info('handler: Request was handled', ['request' => $request->all(), 'result'=>'ok']);
         $result = $request->all();
         return view('bitrix.apps.handler', compact('result'));
     }
