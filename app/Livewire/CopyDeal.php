beab967196057ee2bc0b3424a4c8b9a9184c4d54
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 15:32:20 +0300

    Refactor dropdown menu for better UX
    Replaced the inline-block button with a more interactive and accessible flex-based dropdown. Integrated Alpine.js for smooth toggle and close animations, and updated the dropdown options to include 'New Task', 'Edit Task', and 'Delete Task', enhancing user interaction and visual consistency.

--- app/Livewire/CopyDeal.php
+++ app/Livewire/CopyDeal.php
@@ -7,6 +7,13 @@ use Livewire\Component;
 class CopyDeal extends Component
 {
     public $type = NULL;
+
+    public $cases = [
+        'A' => 'Перекріпити майстра',
+        'B' => 'Допродана угода',
+        'C' => 'Друга послуга',
+        'D' => 'Друге звернення клієнта',
+    ];
     public function render()
     {
         return view('livewire.copy-deal');
--- resources/views/livewire/copy-deal.blade.php
+++ resources/views/livewire/copy-deal.blade.php
@@ -1,8 +1,13 @@
-<div class="flex justify-center">
+<div class="flex justify-center w-50">
     <select class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" wire:model="type" wire:change="changeType">
         <option selected="">Open this select menu</option>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
+        <option value="A">1</option>
+        <option value="B">1</option>
+        <option value="C">1</option>
+        <option value="D">1</option>
     </select>
+
+    @if($type)
+        {{$caces[$type]}}
+    @endif
 </div>
