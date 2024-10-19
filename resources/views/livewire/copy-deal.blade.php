db216662fee321c7fd805e2990a293b44a883ba5
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 18:08:30 +0300

    Fix step condition in copy-deal view
    Corrected the conditional check for the $step variable in the Livewire copy-deal view. Changed assignment operators to equality operators to ensure proper condition evaluation.

--- resources/views/livewire/copy-deal.blade.php
+++ resources/views/livewire/copy-deal.blade.php
@@ -1,6 +1,6 @@
 <div class="flex justify-center w-50">
 
-    @if($step = 0)
+    @if($step == 0)
         <select class="bx-ui-sls-input form-control py-3 px-4 block w-full border-gray-300 rounded text-sm focus:border-blue-300 focus:ring-blue-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" wire:model="type" wire:change="changeType">
             <option selected="" class="bx-ui-sls-placeholder">Open this select menu</option>
             <option value="A" class="bx-ui-sls-option">{{$cases['A']}}</option>
@@ -11,7 +11,7 @@
     @endif
 
 
-    @if($step = 2)
+    @if($step == 2)
         <div class="bx-ui-sls-selected">{{$cases[$type]}}</div>
     @endif
 
