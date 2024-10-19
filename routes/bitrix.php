c03390e1ecb1c94c435d37adde7bb8ff4882db6e
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 10:13:51 +0300

    Allow multiple HTTP methods for Bitrix install route
    Changed the HTTP method for the /bitrix/install endpoint from GET to ANY. This enables the route to accept various types of HTTP requests, enhancing flexibility and integration capabilities.

--- routes/bitrix.php
+++ routes/bitrix.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Bitrix\Apps\MainAppController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/bitrix/install', [MainAppController::class, 'install'])->name('bitrix.main-app.install');
+Route::any('/bitrix/install', [MainAppController::class, 'install'])->name('bitrix.main-app.install');
 Route::any('/bitrix/main-app/handler', [MainAppController::class, 'handler'])->name('bitrix.main-app.handler');
 
 
