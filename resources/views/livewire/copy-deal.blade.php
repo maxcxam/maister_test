ff7d6e47a08d63302c77ba65a2dc2551f78fb3d7
roman <romandzindzyura@gmail.com>
2024-10-19 17:11:50 +0300

    Refactor select element classes for consistency
    Updated the select element and its options to use a unified class naming convention for better consistency and maintainability. Also, wrapped the selected case output in a new class to enhance styling control.

--- resources/views/livewire/copy-deal.blade.php
+++ resources/views/livewire/copy-deal.blade.php
@@ -1,13 +1,13 @@
 <div class="flex justify-center w-50">
-    <select class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" wire:model="type" wire:change="changeType">
-        <option selected="">Open this select menu</option>
-        <option value="A">{{$cases['A']}}</option>
-        <option value="B">{{$cases['B']}}</option>
-        <option value="C">{{$cases['C']}}</option>
-        <option value="D">{{$cases['D']}}</option>
+    <select class="bx-ui-sls-input form-control py-3 px-4 block w-full border-gray-300 rounded text-sm focus:border-blue-300 focus:ring-blue-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" wire:model="type" wire:change="changeType">
+        <option selected="" class="bx-ui-sls-placeholder">Open this select menu</option>
+        <option value="A" class="bx-ui-sls-option">{{$cases['A']}}</option>
+        <option value="B" class="bx-ui-sls-option">{{$cases['B']}}</option>
+        <option value="C" class="bx-ui-sls-option">{{$cases['C']}}</option>
+        <option value="D" class="bx-ui-sls-option">{{$cases['D']}}</option>
     </select>
 
     @if($type)
-        {{$cases[$type]}}
+        <div class="bx-ui-sls-selected">{{$cases[$type]}}</div>
     @endif
 </div>
