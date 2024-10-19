34d7ee604ce0e9eb1359a7fe7d00b792b44886d8
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 18:40:38 +0300

    Refactor constructor to allow nullable dealId.
    Changed the constructor to use a nullable public property for dealId instead of requiring it during instantiation. This adjustment simplifies handling cases where dealId might not be immediately available.

--- app/Services/BitrixTaskManager.php
+++ app/Services/BitrixTaskManager.php
@@ -9,7 +9,8 @@ class BitrixTaskManager
     public static function createCopyDeal(int $dealId, mixed $type)
     {
         $parentDeal = CRest::call('crm.deal.get', ['id' => $dealId]);
-        (new TelegramService())->sendJsonFile($parentDeal);
+        $contacts = CRest::call('crm.deal.contact.items.get', ['id' => $dealId]);
+        (new TelegramService())->sendJsonFile([$parentDeal['result'], $contacts['result']]);
         $newDeal = $parentDeal['result'];
     }
 }
