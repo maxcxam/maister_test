9332f1b777c746ced9b728bcb60272754273ff82
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 10:13:33 +0300

    Change handler route method to accept any HTTP verb
    The route for '/bitrix/main-app/handler' has been updated to use Route::any instead of Route::get. This allows the handler method in MainAppController to accept requests with any HTTP verb, enhancing flexibility.

--- routes/bitrix.php
+++ routes/bitrix.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Bitrix\Apps\MainAppController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/bitrix/install', [MainAppController::class, 'install'])->name('bitrix.main-app.install');
-Route::get('/bitrix/main-app/handler', [MainAppController::class, 'handler'])->name('bitrix.main-app.handler');
+Route::any('/bitrix/main-app/handler', [MainAppController::class, 'handler'])->name('bitrix.main-app.handler');
 
 
 
