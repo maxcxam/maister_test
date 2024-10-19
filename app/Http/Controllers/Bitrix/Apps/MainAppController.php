3ec258fa88fb0df8a15b8bec3e79f1016f00435e
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 11:46:35 +0300

    Update Request import in MainAppController
    Replaced 'Illuminate\Support\Facades\Request' with 'Illuminate\Http\Request' to ensure correct request handling and avoid potential namespace conflicts. This change will improve the code's readability and maintainability.

--- app/Http/Controllers/Bitrix/Apps/MainAppController.php
+++ app/Http/Controllers/Bitrix/Apps/MainAppController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Bitrix\Apps;
 use App\B24\CRest;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class MainAppController extends Controller
 {
