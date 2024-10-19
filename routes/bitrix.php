b2aeb7c08b4972e96c3405570771b743e0afb005
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 11:29:59 +0300

    Exclude CSRF middleware in Bitrix routes
    Added a route group to exclude the CSRF middleware for Bitrix install and handler routes. This change ensures the respective routes function correctly without the CSRF token verification.

--- routes/bitrix.php
+++ routes/bitrix.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['excluded_middleware' => VerifyCsrfToken::class], function () {
     Route::any('/bitrix/install', [MainAppController::class, 'install'])->withoutMiddleware(VerifyCsrfToken::class)->name('install');
     Route::any('/bitrix/main-app/handler', [MainAppController::class, 'handler'])->withoutMiddleware(VerifyCsrfToken::class)->name('handler');
-})->name('bitrix.main-app;
-
+})->name('bitrix.main-app');
 
 
 
