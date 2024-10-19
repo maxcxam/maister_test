a529f14def53c62fd93a4d8d3917a2a64dfa3551
Maksym Rudenko <maxcxam1979@gmail.com>
2024-10-19 12:00:43 +0300

    Update Request import in MainAppController
    Replaced 'Illuminate\Support\Facades\Request' with 'Illuminate\Http\Request' to ensure correct request handling and avoid potential namespace conflicts. This change will improve the code's readability and maintainability.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(AllowIFrameMiddleware::class);
+        // $middleware->append(AllowIFrameMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
